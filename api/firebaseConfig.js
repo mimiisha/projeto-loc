9afac6eaf89ff73e09ddaa4dea389413745af92a
Misha <dan.mcordeiro@gmail.com>
2025-11-18 03:52:58 -0300

    add projeto git

---
+++ api/firebaseConfig.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyCH0UB9qkad2bRH7fVG4A81U45zoJIWLVY",
+  authDomain: "misholas.firebaseapp.com",
+  projectId: "misholas",
+  storageBucket: "misholas.firebasestorage.app",
+  messagingSenderId: "180628915497",
+  appId: "1:180628915497:web:8c2a5b1870be9237f12338",
+  measurementId: "G-VRC36B68X7"
+};
