0dedb86c1eef9121f2d8c37e37f1888a0a8ead46
Misha <dan.mcordeiro@gmail.com>
2025-11-18 04:00:18 -0300

    firebase

--- api/firebaseConfig.js
+++ api/firebaseConfig.js
@@ -1,9 +1,17 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyCH0UB9qkad2bRH7fVG4A81U45zoJIWLVY",
-  authDomain: "misholas.firebaseapp.com",
-  projectId: "misholas",
-  storageBucket: "misholas.firebasestorage.app",
-  messagingSenderId: "180628915497",
-  appId: "1:180628915497:web:8c2a5b1870be9237f12338",
-  measurementId: "G-VRC36B68X7"
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  };
+
+  return res.status(200).json(firebaseConfig);
 };
